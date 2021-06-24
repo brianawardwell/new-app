0600ef10ddf2b16b21a4fdeba45f9c7101578ce8
Briana Wardwell <brianawardwell@yahoo.com>
2021-06-23 20:57:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Briana Wardwell</h1>
+        <p>This is my react app hosted web site on Git Hub</p>
       </header>
     </div>
   );
 }
+
 
 export default App;
